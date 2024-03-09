896ccee090e107f98bc18ca6c9814a8eebf7077c
useri52og5i7e <player364xl@gmail.com>
2024-03-09 05:55:32 -0800

    Рефакторинг кода

--- inventory.go
+++ inventory.go
@@ -20,7 +20,7 @@ var (
 	stoneCount    int  = 0
 	metalCount    int  = 0
 	wallIsOpen    bool = true
-	floorIsOpen   bool
+	floorIsOpen   bool = true
 	question      rl.Texture2D
 )
 
@@ -28,6 +28,14 @@ type InventorySlot struct {
 	x          float32
 	y          float32
 	slotNumber int
+}
+
+func createInventoryRow(startX, startY float32, slots, spacing int, inventory *[]InventorySlot, slotNum *int) {
+	for i := 0; i < slots; i++ {
+		slotX := startX + float32(i)*float32(slotImage.Width*int32(cam.Zoom)+int32(spacing))
+		(*inventory)[*slotNum] = newInventorySlot(slotX, startY, *slotNum)
+		*slotNum++
+	}
 }
 
 func loadInventory() {
@@ -48,31 +56,11 @@ func loadInventory() {
 	startX1 := inventoryLabelPos.X + 50
 	inventorySlotNum := 0
 	hotInventorySlotNum := 0
-	firstSlot := startX1 - 200
-	for i := 0; i < 3; i++ {
-		slotX := startX1 + float32(i)*float32((slotImage.Width*int32(cam.Zoom))+110)
-		inventory[inventorySlotNum] = newInventorySlot(slotX, 182.0, inventorySlotNum)
-		inventorySlotNum++
-	}
-	for i := 0; i < 8; i++ {
-		slotX := firstSlot + float32(i)*float32((slotImage.Width*int32(cam.Zoom))+32)
-		hotInventory[hotInventorySlotNum] = newInventorySlot(slotX, 395.0, hotInventorySlotNum)
-		hotInventorySlotNum++
-	}
-	for i := 0; i < 8; i++ {
-		slotX := firstSlot + float32(i)*float32((slotImage.Width*int32(cam.Zoom))+32)
-		hotInventory[hotInventorySlotNum] = newInventorySlot(slotX, 495.0, hotInventorySlotNum)
-		hotInventorySlotNum++
-	}
-	for i := 0; i < 8; i++ {
-		slotX := firstSlot + float32(i)*float32((slotImage.Width*int32(cam.Zoom))+32)
-		hotInventory[hotInventorySlotNum] = newInventorySlot(slotX, 595.0, hotInventorySlotNum)
-		hotInventorySlotNum++
-	}
-	for i := 0; i < 8; i++ {
-		slotX := firstSlot + float32(i)*float32((slotImage.Width*int32(cam.Zoom))+32)
-		hotInventory[hotInventorySlotNum] = newInventorySlot(slotX, 695.0, hotInventorySlotNum)
-		hotInventorySlotNum++
+
+	createInventoryRow(startX1, 182.0, 3, 110, &inventory, &inventorySlotNum)
+	yPositions := []float32{395.0, 495.0, 595.0, 695.0}
+	for _, yPos := range yPositions {
+		createInventoryRow(startX1-200, yPos, 8, 32, &hotInventory, &hotInventorySlotNum)
 	}
 }
 
@@ -90,6 +78,22 @@ func newInventorySlot(x, y float32, slotNumber int) InventorySlot {
 		x:          x,
 		y:          y,
 		slotNumber: slotNumber,
+	}
+}
+
+func drawSlot(slot int, open bool) {
+	if open {
+		slotCenterX := hotInventory[slot].x + (float32(slotImage.Width)/2)*cam.Zoom
+		slotCenterY := hotInventory[slot].y + (float32(slotImage.Height)/2)*cam.Zoom
+		itemPosX := slotCenterX - (float32(otherTextures[slot].Width)/2)*cam.Zoom
+		itemPosY := slotCenterY - (float32(otherTextures[slot].Height)/2)*cam.Zoom
+		rl.DrawTextureEx(otherTextures[slot], rl.NewVector2(itemPosX, itemPosY), 0, cam.Zoom, rl.White)
+	} else {
+		slotCenterX := hotInventory[slot].x + (float32(slotImage.Width)/2)*cam.Zoom
+		slotCenterY := hotInventory[slot].y + (float32(slotImage.Height)/2)*cam.Zoom
+		itemPosX := slotCenterX - (float32(textures[slot].Width)/2)*cam.Zoom
+		itemPosY := slotCenterY - (float32(textures[slot].Height)/2)*cam.Zoom
+		rl.DrawTextureEx(question, rl.NewVector2(itemPosX, itemPosY), 0, cam.Zoom, rl.White)
 	}
 }
 
@@ -130,30 +134,14 @@ func drawItems() {
 			rl.DrawTextEx(font, itemCountText, rl.NewVector2(itemCountPosX, itemCountPosY), fontSize, fontSpacing, rl.White)
 		}
 		if wallIsOpen {
-			slotCenterX := hotInventory[0].x + (float32(slotImage.Width)/2)*cam.Zoom
-			slotCenterY := hotInventory[0].y + (float32(slotImage.Height)/2)*cam.Zoom
-			itemPosX := slotCenterX - (float32(textures[i].Width)/2)*cam.Zoom
-			itemPosY := slotCenterY - (float32(textures[i].Height)/2)*cam.Zoom
-			rl.DrawTextureEx(wall, rl.NewVector2(itemPosX, itemPosY), 0, cam.Zoom, rl.White)
+			drawSlot(0, true)
 		} else {
-			slotCenterX := hotInventory[0].x + (float32(slotImage.Width)/2)*cam.Zoom
-			slotCenterY := hotInventory[0].y + (float32(slotImage.Height)/2)*cam.Zoom
-			itemPosX := slotCenterX - (float32(textures[i].Width)/2)*cam.Zoom
-			itemPosY := slotCenterY - (float32(textures[i].Height)/2)*cam.Zoom
-			rl.DrawTextureEx(question, rl.NewVector2(itemPosX, itemPosY), 0, cam.Zoom, rl.White)
+			drawSlot(0, false)
 		}
 		if floorIsOpen {
-			slotCenterX := hotInventory[1].x + (float32(slotImage.Width)/2)*cam.Zoom
-			slotCenterY := hotInventory[1].y + (float32(slotImage.Height)/2)*cam.Zoom
-			itemPosX := slotCenterX - (float32(textures[i].Width)/2)*cam.Zoom
-			itemPosY := slotCenterY - (float32(textures[i].Height)/2)*cam.Zoom
-			rl.DrawTextureEx(floor, rl.NewVector2(itemPosX, itemPosY), 0, cam.Zoom, rl.White)
+			drawSlot(1, true)
 		} else {
-			slotCenterX := hotInventory[1].x + (float32(slotImage.Width)/2)*cam.Zoom
-			slotCenterY := hotInventory[1].y + (float32(slotImage.Height)/2)*cam.Zoom
-			itemPosX := slotCenterX - (float32(textures[i].Width)/2)*cam.Zoom
-			itemPosY := slotCenterY - (float32(textures[i].Height)/2)*cam.Zoom
-			rl.DrawTextureEx(question, rl.NewVector2(itemPosX, itemPosY), 0, cam.Zoom, rl.White)
+			drawSlot(1, false)
 		}
 	}
 }
