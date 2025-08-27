1f6bb2828bd2e0326c4249e18739c3d0790e00e4
otie173 <otie173.email@gmail.com>
2025-08-27 23:15:04 +0300

    added ticker in server

---
+++ internal/server/ticker/ticker.go
@@ -0,0 +1,33 @@
+package ticker
+
+import (
+	"log"
+	"time"
+)
+
+type Ticker struct {
+	tps    int
+	onTick func()
+}
+
+func New(tps int, onTick func()) *Ticker {
+	return &Ticker{
+		tps:    tps,
+		onTick: onTick,
+	}
+}
+
+func (t *Ticker) Run() {
+	tickDuration := time.Second / time.Duration(t.tps)
+	ticker := time.NewTicker(tickDuration)
+
+	for range ticker.C {
+		start := time.Now()
+		t.onTick()
+		elapsed := time.Since(start)
+
+		if elapsed > tickDuration {
+			log.Printf("Warning! Tick took too long: %v", elapsed)
+		}
+	}
+}
