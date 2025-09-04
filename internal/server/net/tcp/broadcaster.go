45f8e544af651dc87d0c6c2310c1ff040ed89b3c
otie173 <otie173.email@gmail.com>
2025-09-05 02:17:20 +0300

    added minimal broadcast function to broadcaster package

--- internal/server/net/tcp/broadcaster.go
+++ internal/server/net/tcp/broadcaster.go
@@ -1,7 +1,13 @@
 package tcp
 
+import "github.com/otie173/odinbit/internal/server/game/player"
+
 type Broadcaster struct{}
 
 func NewBroadcaster() *Broadcaster {
 	return &Broadcaster{}
+}
+
+func (b *Broadcaster) Broadcast(data []byte, players []player.Player) {
+
 }
